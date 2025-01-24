942b7fc3a976bb679a4c4d80f87f51c9f99e01e7
Adrianna Grzegorczyk <ada.grzegorczyk@gmail.com>
2025-01-24 14:56:27 +0100

    Fixed printing

--- asciimirror/Main.java
+++ asciimirror/Main.java
@@ -1,7 +1,24 @@
 package asciimirror;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input the file path:");
+        String input = scanner.nextLine();
+        String[] cowLines = {
+                input+"\n"+
+                        "            ^__^",
+                "    _______/(oo)",
+                "/\\/(       /(__)",
+                "   | w----||    ",
+                "   ||     ||    "
+        };
+
+        for (String line : cowLines) {
+            System.out.println(line);
+        }
     }
 }
