c5edefe1c3d4de55cd4cb08788958e8fdfa63650
Adrianna Grzegorczyk <ada.grzegorczyk@gmail.com>
2025-01-25 18:30:15 +0100

    Added option for read files

--- asciimirror/Main.java
+++ asciimirror/Main.java
@@ -1,24 +1,37 @@
 package asciimirror;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // Creating BufferedReader for Input
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Input the file path:");
-        String input = scanner.nextLine();
-        String[] cowLines = {
-                input+"\n"+
-                        "            ^__^",
-                "    _______/(oo)",
-                "/\\/(       /(__)",
-                "   | w----||    ",
-                "   ||     ||    "
-        };
 
-        for (String line : cowLines) {
-            System.out.println(line);
+        try {
+            BufferedReader bfri = new BufferedReader(
+                    new InputStreamReader(System.in));
+
+            System.out.print("Input the file path:");
+
+
+            String path = bfri.readLine();
+
+
+            BufferedReader bfro = new BufferedReader(
+                    new FileReader(path));
+
+
+            String st;
+
+            while ((st = bfro.readLine()) != null)
+
+                System.out.println(st);
+
+        } catch (IOException e) {
+            System.out.println("File not found!");
+
+
         }
     }
 }
